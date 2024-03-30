1367f720f32ccba62bb2b85b04bd32d921b7a6e1
Hunting378 <140703804+Hunting378@users.noreply.github.com>
2024-03-30 19:03:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
-
-setInterval(() => {
-  fetch(`https://nia-statistics.com/api/get?platform=youtube&type=channel&id=${id}`).then((res) => res.json()).then((data) => {
-                     document.getElementById("counter").innerHTML = data.estSubCount;
-
-    document.getElementById("userimg").src = data.snippet.thumbnails[2].url;
-    document.getElementById("userName").innerHTML = data.snippet.title;
-    if (chart.series[0].points.length == 1500) chart.series[0].data[0].remove();
-    chart.series[0].addPoint([Date.now(), data.estSubCount])
-                  });
-      }, 2000);
-
-document.getElementById(
-  "subscribeBtn"
-).href = `https://youtube.com/channel/${id}?sub_confirmation=1`;
-
-function search() {
-  const prompt = window.prompt("Enter channel name, ID, or URL.");
-  if (prompt)
-    fetch(
-      `https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        window.location.href = "?id=" + data[0].id;
-      });
-}
-
 var textBright = "#bdbdbd"
 var lineColor = "#000000"
 var maxPoints = 20000;
@@ -76,3 +48,30 @@ var chart = new Highcharts.chart({
         lineColor: '#b3382c'
     }]
 });
+
+setInterval(() => {
+  fetch(`https://nia-statistics.com/api/get?platform=youtube&type=channel&id=${id}`).then((res) => res.json()).then((data) => {
+                     document.getElementById("counter").innerHTML = data.estSubCount;
+
+    document.getElementById("userimg").src = data.snippet.thumbnails[2].url;
+    document.getElementById("userName").innerHTML = data.snippet.title;
+    if (chart.series[0].points.length == 1500) chart.series[0].data[0].remove();
+    chart.series[0].addPoint([Date.now(), data.estSubCount])
+                  });
+      }, 2000);
+
+document.getElementById(
+  "subscribeBtn"
+).href = `https://youtube.com/channel/${id}?sub_confirmation=1`;
+
+function search() {
+  const prompt = window.prompt("Enter channel name, ID, or URL.");
+  if (prompt)
+    fetch(
+      `https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        window.location.href = "?id=" + data[0].id;
+      });
+}
